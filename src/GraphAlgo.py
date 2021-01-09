c4e5c315c9e0f55382c21ea4d34bf068ef36b974
Shani Itzhakov <shani.it96@gmail.com>
2021-01-10 00:04:57 +0200

    Added comments to GraphAlgo

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -1,5 +1,4 @@
 from typing import List
-import heapq as q
 from queue import PriorityQueue
 from src import GraphInterface
 from src.NodeData import NodeData
@@ -11,9 +10,13 @@ import matplotlib.pyplot as plt
 
 
 class GraphAlgo:
-    """This abstract class represents an interface of a graph."""
+    """This abstract class represents algorithms functions, save and load to json and plot of a graph."""
 
     def __init__(self, graph=DiGraph()):
+        """
+        Constructor
+        @param graph: the graph of DiGraph
+        """
         self.graph = graph
 
     def get_graph(self) -> GraphInterface:
@@ -127,8 +130,8 @@ class GraphAlgo:
         # If id1 is equal to id2, returns distance of 0 and a list with one of the node ids
         if id1 is id2:
             return 0, [id2]
-        # Using dijkstra algorithm with id1 and id2 to receive the distance
-        self.dijkstra(id1, id2)
+        # Using dijkstra algorithm with id1 to receive the distance of the shortest path
+        self.dijkstra(id1)
         # If the tag of id2 node is equal to infinity it was not visited, returns distance of infinity and an empty list
         if all_nodes[id2].tag is math.inf:
             return math.inf, []
@@ -154,36 +157,51 @@ class GraphAlgo:
         If the graph is None or id1 is not in the graph, the function should return an empty list []
         """
         connected = []
+        # If graph is empty, returns an empty list
         if self.graph is None:
             return connected
         all_nodes_original = self.graph.get_all_v()
+        # If there are no nodes in the graph or all nodes is empty
+        # or id1 does not exists in the graph, returns an empty list
         if all_nodes_original is None or not all_nodes_original or all_nodes_original[id1] is None:
             return connected
 
         original_list = []
         copy_list = []
+        # Using dijkstra on id1 as a starting node to go over the graph
         self.dijkstra(id1)
         original_graph = self.graph
+        # Create a new graph as a copy_graph to reverse the edges to find the strongly connected nodes
         copy_graph = DiGraph()
+        # Loop over the nodes in the original graph and add the key of the node to the copy_graph
         for key, node in all_nodes_original.items():
             copy_graph.add_node(key)
+            # If the node tag is less then infinity, then add the key to the list, because the node was passed though
             if node.tag < math.inf:
                 original_list.append(key)
-
+        # Loop over all the nodes in the original graph
         for src in all_nodes_original:
+            # Loop over all the out edges of a node to get its dest node and the weight between them
             for dest, weight in original_graph.all_out_edges_of_node(src).items():
+                # add an edge backwards from dest to src with the weight between them
                 copy_graph.add_edge(dest, src, weight)
 
+        # Copy the copy_graph to the graph to use dijkstra on id1
         self.graph = copy_graph
         self.dijkstra(id1)
 
+        # Loop over the nodes in the copy_graph
         for key, node in copy_graph.get_all_v().items():
+            # If the node tag is less then infinity, then add the key of the node to the copy_list
             if node.tag < math.inf:
                 copy_list.append(key)
 
+        # Loop over the copy_list
         for key in copy_list:
+            # If the key of the copy_list is in the original list, then add the key to the connected list
             if key in original_list:
                 connected.append(key)
+        # Copy the original graph to the graph
         self.graph = original_graph
         return connected
 
@@ -196,19 +214,28 @@ class GraphAlgo:
         """
 
         connected_components = []
+        # If graph is empty, returns an empty list
         if self.graph is None:
             return connected_components
 
         all_nodes = self.graph.get_all_v()
+        # If there are no nodes in the graph or all nodes is empty, returns an empty list
         if all_nodes is None or not all_nodes:
             return connected_components
 
+        # Create a set of the nodes that were passed though in the graph
         check_set = set()
+        # Loop over all the node keys in the graph
         for key in all_nodes:
+            # If the connected_components list is empty or if the key does not exists in the set,
+            # then use connected_component function on the key and add it to a list of strongly connected nodes
             if not connected_components or key not in check_set:
                 connected_list = self.connected_component(key)
+                # If the strongly connected nodes list exists, then add to the final list
+                # the strongly connected nodes from the connected_list
                 if connected_list:
                     connected_components.append(connected_list)
+                    # Change the list to a set, and put in check_set
                     check_set |= set(connected_list)
         return connected_components
 
@@ -224,36 +251,51 @@ class GraphAlgo:
         z_list = []
         pos_dict = {}
         all_nodes = self.graph.get_all_v()
+        # Use min_max function to get the min and max of x, y, z
         x_min, y_min, z_min, x_max, y_max, z_max = self.get_min_max()
+        # Loop over all the nodes in the graph
         for key, node in all_nodes.items():
+            # If the node pos exists, then add the pos of the nodes to x, y, z lists
             if node.pos is not None:
                 x, y, z = node.pos
                 x_list.append(x)
                 y_list.append(y)
                 z_list.append(z)
+                # Put in the pos dictionary in key the node pos value
                 pos_dict[key] = node.pos
+            # If the node pos does not exists, use random function  on x, y, z min and max
             else:
                 x_val = rand.uniform(x_min, x_max)
                 y_val = rand.uniform(y_min, y_max)
                 z_val = rand.uniform(z_min, z_max)
+                # Add the x, y, z random min and max values to x, y, z lists
                 x_list.append(x_val)
                 y_list.append(y_val)
                 z_list.append(z_val)
+                # Put in pos dictionary in key, the x, y, z random min and max values
                 pos_dict[key] = x_val, y_val, z_val
 
+        # Create the panel and frame to paint on
         fig, ax = plt.subplots(figsize=(10, 5))
+        # Add the nodes from the lists to the plot
         ax.plot(x_list, y_list, ls="", marker="o", color='red')
+        # Loop over with iterator on x, y, key from the tuple, and paint the keys over the nodes
         for x, y, key in zip(x_list, y_list, all_nodes):
             ax.annotate(str(key), xy=(x, y), fontsize=15, color='green')
 
+        # Loop over all the node keys in the graph
         for key in all_nodes:
+            # Loop over all the out edges of a node to get the dest and the weight between them
             for dest, weight in self.graph.all_out_edges_of_node(key).items():
+                # Put in x, y, z src and dest the src key and dest from the pos dictionary
                 x_src, y_src, z_src = pos_dict[key]
                 x_dest, y_dest, z_dest = pos_dict[dest]
+                # Making an arrow between the nodes to point the in edges and out edges
                 ax.arrow(x_src, y_src, x_dest - x_src, y_dest - y_src, head_width=0.0003, head_length=0.0003, fc='k',
                          ec='k',
                          length_includes_head=True, width=0.00005)
 
+        # Add a title to the plot and show the graph
         plt.title("Graph Plot")
         plt.show()
 
@@ -281,16 +323,26 @@ class GraphAlgo:
                     # return node.tag
 
     def get_min_max(self) -> (float, float, float, float, float, float):
+        """
+        Get the minimum and maximum of the positions of nodes in the graph.
+        If the nodes have a position, the nodes then returns the minimum and maximum of x, y, z positions.
+        Otherwise, they will be placed in a random but elegant manner.
+        @return: tuple of (float, float, float, float, float, float)
+        """
         all_nodes = self.graph.get_all_v()
         x_list = []
         y_list = []
         z_list = []
+        # Loop over the nodes in the graph
         for key, node in all_nodes.items():
+            # If node pos exists, then add the pos of x, y, z to their lists
             if node.pos is not None:
                 x, y, z = node.pos
                 x_list.append(x)
                 y_list.append(y)
                 z_list.append(z)
+        # If the lists are empty, returns basic values to min and max of x, y, z
         if not x_list or not y_list:
             return 0, 0, 0, 10, 10, 0
+        # Using min and max functions to return the min and max of x,y,z from their lists
         return min(x_list), min(y_list), min(z_list), max(x_list), max(y_list), max(z_list)
